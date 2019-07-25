8c8083b443dd97d1f34295b611edd44df96406f5
Diego T. Fialho <diego.tfialho@gmail.com>
2019-07-25 18:17:41 -0300

    Navbar implementada

---
+++ src/components/navbar.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Link } from "gatsby"
+import Logo from "../components/logo"
+import "../styles/navbar.scss";
+
+const Navbar = () => (
+  <nav className="navbar">
+    <Logo />
+    
+    <ul className="navbar__container">
+      <li className="navbar__item">
+        <Link
+          to="/"
+          activeClassName="active">
+          Home
+        </Link>
+      </li>
+      <li className="navbar__item">
+        <Link
+          to="about-me"
+          activeClassName="active">
+          Sobre Mim
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Navbar
