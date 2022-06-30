d8cda2994934d0105261f00a6a053c39d5c00f56
Diego T. Fialho <diego.tfialho@gmail.com>
2022-06-30 18:06:43 -0300

    chore: add shell script to init article

---
+++ init-article.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p "Enter the file name (it will also be the slug): " slug
+read -p "Enter the article title: " title
+read -p "Enter the article excerpt: " excerpt
+read -p "Enter the image extension (extension only without dot): " image
+now=$(date +%Y-%m-%dT%H:%M:%S%z)
+
+file="./posts/$slug.md"
+
+touch $file
+echo "---" > $file
+echo "title: '${title}'" >> $file
+echo "date: '${now}'" >> $file
+echo "hero_image: '${slug}.${image}'" >> $file
+echo "excerpt: '${excerpt}'" >> $file
+echo "---" >> $file
