6d473e1151c7fe92441a05d2fe5c306c8668442b
Diego T. Fialho <diego.tfialho@gmail.com>
2022-07-19 11:15:59 -0300

    feat: update links to use next/link

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Image from 'next/image'
 import format from 'date-fns/format'
 
@@ -21,24 +22,26 @@ const Post = ({
   imgPriority
 }: PostType) => (
   <S.Wrapper>
-    <S.Link href={`/blog/${slug}`} title={title}>
-      <S.ImageWrapper>
-        <Image
-          src={image}
-          layout="fill"
-          objectFit="cover"
-          alt={title}
-          priority={imgPriority}
-        />
-      </S.ImageWrapper>
-      <S.Content>
-        <S.Title>{title}</S.Title>
-        <S.Description>{excerpt}</S.Description>
-        <S.Date>
-          Em: <span>{format(new Date(date), 'dd/MM/yyyy')}</span>
-        </S.Date>
-      </S.Content>
-    </S.Link>
+    <Link href={`/blog/${slug}`} passHref>
+      <S.Link title={title}>
+        <S.ImageWrapper>
+          <Image
+            src={image}
+            layout="fill"
+            objectFit="cover"
+            alt={title}
+            priority={imgPriority}
+          />
+        </S.ImageWrapper>
+        <S.Content>
+          <S.Title>{title}</S.Title>
+          <S.Description>{excerpt}</S.Description>
+          <S.Date>
+            Em: <span>{format(new Date(date), 'dd/MM/yyyy')}</span>
+          </S.Date>
+        </S.Content>
+      </S.Link>
+    </Link>
   </S.Wrapper>
 )
 
