58dbdb82df736e7e5957b69120db06c5b6124021
Diego T. Fialho <diego.tfialho@gmail.com>
2022-08-08 09:35:54 -0300

    feat: sort all posts by date

--- src/lib/get-all-posts-by-locale.ts
+++ src/lib/get-all-posts-by-locale.ts
@@ -1,23 +1,31 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import compareDesc from 'date-fns/compareDesc'
 
 const getAllPostsByLocale = (locale: string) => {
   const dir = locale === 'pt-BR' ? 'default' : locale
   const postsFolder = path.join('posts', dir)
   const files = fs.readdirSync(postsFolder)
 
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '')
-    const filePath = path.join('posts', dir, filename)
-    const markdown = fs.readFileSync(filePath)
-    const { data } = matter(markdown)
+  const posts = files
+    .map((filename) => {
+      const slug = filename.replace('.md', '')
+      const filePath = path.join('posts', dir, filename)
+      const markdown = fs.readFileSync(filePath)
+      const { data } = matter(markdown)
 
-    return {
-      slug,
-      ...data
-    }
-  })
+      return {
+        slug,
+        date: '',
+        ...data
+      }
+    })
+    .sort((first, second) => {
+      const firstDate = new Date(first.date)
+      const secondDate = new Date(second.date)
+      return compareDesc(firstDate, secondDate)
+    })
 
   return posts
 }
