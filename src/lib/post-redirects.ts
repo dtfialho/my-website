7aa841462fe20411c15a5f853c315a4fcf1886b3
Diego T. Fialho <diego.tfialho@gmail.com>
2022-08-11 14:37:47 -0300

    feat: add next-sitemap

--- src/lib/post-redirects.ts
+++ src/lib/post-redirects.ts
@@ -7,12 +7,12 @@ type RedirectsTypes = {
 const redirects: RedirectsTypes = {
   'pt-BR': {
     'simple-accordion-with-html-and-css': 'accordion-simples-com-html-e-css',
-    'creating-a-react-application-with-react-without-create-react-app':
+    'creating-a-react-application-without-create-react-app':
       'criando-uma-aplicacao-em-react-sem-create-react-app'
   },
   en: {
     'criando-uma-aplicacao-em-react-sem-create-react-app':
-      'creating-a-react-application-with-react-without-create-react-app',
+      'creating-a-react-application-without-create-react-app',
     'accordion-simples-com-html-e-css': 'simple-accordion-with-html-and-css'
   }
 }
