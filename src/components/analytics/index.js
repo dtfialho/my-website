4529b834f257003cfc5b4b567613706ba6d511df
Diego T. Fialho <diego.tfialho@gmail.com>
2023-05-19 09:52:17 -0300

    feat: add vercel analytics

--- src/components/analytics/index.js
+++ src/components/analytics/index.js
@@ -1,10 +1,12 @@
 import Script from 'next/script'
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
 
 const Analytics = () => {
   const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING
 
   return (
     <>
+      <VercelAnalytics />
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
