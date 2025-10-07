de635e2ac830e25981dc96b1ae256c14db8549bd
Diego T. Fialho <diego.tfialho@gmail.com>
2025-10-07 11:35:03 -0300

    feat: update home

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,30 @@
+import { getTranslations } from 'next-intl/server'
+
+import Template from 'templates/home'
+
+export async function generateMetadata() {
+  const t = await getTranslations()
+
+  const title = 'Home | Diego T. Fialho'
+  const description = t('Home.description')
+  const keywords = t('Home.keywords')
+  const url = 'https://www.diegotfialho.dev'
+
+  return {
+    keywords,
+    title,
+    description,
+    canonical: url,
+    openGraph: {
+      site_name: 'Diego T. Fialho',
+      url,
+      title,
+      description,
+      images: [{ url: 'https://www.diegotfialho.dev/img/icon-512x512.png' }]
+    }
+  }
+}
+
 export default function RootPage() {
-  return 'Hello, World!'
+  return <Template />
 }
