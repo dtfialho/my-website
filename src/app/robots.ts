687d33eb5f274b1f5950cdd59088ecf30a02d658
Diego T. Fialho <diego.tfialho@gmail.com>
2025-11-07 11:01:46 -0300

    feat: add host in robots file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/'
     },
-    sitemap: 'https://www.diegotfialho.dev/sitemap.xml'
+    sitemap: 'https://www.diegotfialho.dev/sitemap.xml',
+    host: 'https://www.diegotfialho.dev'
   }
 }
