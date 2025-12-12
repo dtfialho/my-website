9b6df7d7edb0017bc602f21f38e526ef251772b7
Diego T. Fialho <diego.tfialho@gmail.com>
2025-12-12 11:21:51 -0300

    feat: upgrade and reconfigure prettier with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
- 
+import pluginPrettier from 'eslint-plugin-prettier/recommended'
+
 const eslintConfig = defineConfig([
   ...nextVitals,
-  globalIgnores([
-    '.next/**',
-    'coverage/**',
-    'next-env.d.ts',
-  ]),
+  pluginPrettier,
+  globalIgnores(['.next/**', 'coverage/**', 'next-env.d.ts'])
 ])
- 
+
 export default eslintConfig
