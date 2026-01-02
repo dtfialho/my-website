05095db4e98e3caa240cd28bd238d1adda476f66
Diego T. Fialho <diego.tfialho@gmail.com>
2026-01-02 11:45:29 -0300

    chore: import type

--- src/components/language-selector/index.tsx
+++ src/components/language-selector/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import type { MouseEvent } from 'react'
 import Image from 'next/image'
 import { useContext } from 'react'
 import { useParams } from 'next/navigation'
@@ -12,7 +13,7 @@ const LanguageSelector = () => {
   const { showModal, setShowModal } = useContext(LanguageSelectorContext)
   const { locale: activeLocale } = useParams()
 
-  const handleOpenModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleOpenModal = (e: MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation()
     setShowModal(true)
   }
