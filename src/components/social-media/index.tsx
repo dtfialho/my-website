06aade7007bdaed15a9492c433aae3e67db931a1
Diego T. Fialho <diego.tfialho@gmail.com>
2026-01-13 15:36:17 -0300

    feat: update social media component

--- src/components/social-media/index.tsx
+++ src/components/social-media/index.tsx
@@ -1,43 +1,52 @@
-'use client'
+import Image from 'next/image'
+import clsx from 'clsx'
 
-import { Github, Linkedin, Twitter } from '@styled-icons/fa-brands'
-import * as S from './styles'
+import './styles.css'
 
-const SocialMedia = () => (
-  <S.Container>
-    <S.Item>
-      <S.Link
-        href="https://github.com/dtfialho"
-        target="_blank"
-        rel="noopener noreferrer me"
-        title="Github"
+interface SocialMediaProps {
+  inverseColorToBlack?: boolean
+}
+
+const links = [
+  {
+    name: 'Github',
+    url: 'https://github.com/dtfialho',
+    icon: '/img/github.png'
+  },
+  {
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/diego-teixeira-fialho-35b58ab0/',
+    icon: '/img/linkedin.png'
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/dtfialho',
+    icon: '/img/twitter.png'
+  }
+]
+
+const SocialMedia = ({ inverseColorToBlack = false }: SocialMediaProps) => (
+  <ul className="social-media__container">
+    {links.map(({ name, url, icon }) => (
+      <li
+        className={clsx(
+          'social-media__item',
+          inverseColorToBlack && 'inverse-color'
+        )}
+        key={name}
       >
-        <Github title="Ícone do Github" size={30} />
-      </S.Link>
-    </S.Item>
-
-    <S.Item>
-      <S.Link
-        href="https://www.linkedin.com/in/diego-teixeira-fialho-35b58ab0/"
-        target="_blank"
-        rel="noopener noreferrer me"
-        title="Linkedin"
-      >
-        <Linkedin title="Ícone do Linkedin" size={30} />
-      </S.Link>
-    </S.Item>
-
-    <S.Item>
-      <S.Link
-        href="https://twitter.com/dtfialho"
-        target="_blank"
-        rel="noopener noreferrer me"
-        title="Twitter"
-      >
-        <Twitter title="Ícone do Twitter" size={30} />
-      </S.Link>
-    </S.Item>
-  </S.Container>
+        <a
+          className="social-media__link"
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer me"
+          title={name}
+        >
+          <Image src={icon} alt={name} width={30} height={30} quality={100} />
+        </a>
+      </li>
+    ))}
+  </ul>
 )
 
 export default SocialMedia
--- src/components/social-media/test.tsx
+++ src/components/social-media/test.tsx
@@ -1,23 +1,28 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import SocialMedia from './'
 
 describe('Components/SocialMedia', () => {
+  const user = userEvent.setup()
+
   it('Should render correctly', () => {
     const { container } = render(<SocialMedia />)
 
     expect(container).toMatchSnapshot()
   })
 
-  it('Should link applies opacity on hover', async () => {
-    render(<SocialMedia />)
-    fireEvent.focus(screen.getByRole('link', { name: /Github/ }))
+  it('Should apply inverse class correctly if inverseColorToBlack is true', async () => {
+    render(<SocialMedia inverseColorToBlack />)
 
-    expect(await screen.findByRole('link', { name: /Github/ })).toHaveStyleRule(
-      'opacity',
-      '0.5',
-      {
-        modifier: ':focus'
-      }
-    )
+    expect(
+      screen.getByRole('link', { name: /Github/ }).parentElement
+    ).toHaveClass('inverse-color')
+    expect(
+      screen.getByRole('link', { name: /Linkedin/ }).parentElement
+    ).toHaveClass('inverse-color')
+    expect(
+      screen.getByRole('link', { name: /Twitter/ }).parentElement
+    ).toHaveClass('inverse-color')
   })
 })
